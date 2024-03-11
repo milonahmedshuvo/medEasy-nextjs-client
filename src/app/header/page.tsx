663baea51c3c315b5886f0db75f93af2d6e7c58e
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-03-11 15:42:17 +0600

    layout system

---
+++ src/app/header/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderPage = () => {
+    return (
+        <div>
+            <h1>Header page</h1>
+        </div>
+    );
+};
+
+export default HeaderPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-       <h2 className='text-red-500 text-4xl font-medium'>medicide side medEasy home page </h2>
+       <h2 className='text-red-500 text-2xl font-medium'>Home page </h2>
     </div>
   )
 }
