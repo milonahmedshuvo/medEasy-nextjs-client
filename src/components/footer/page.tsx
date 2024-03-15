b2cbe1f85afa42c134835f52940bdfbc41ac0754
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-03-15 18:05:53 +0600

    line two and four

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -16,7 +16,7 @@ const FooterPage = () => {
             </div>
 
             <div className='w-[250px] border'>
-                <div className='bg-[#25A8D6] w-[70px] h-[60px] rounded-xl flex justify-center items-center'>
+                <div className='bg-[#25A8D6] w-[70px] h-[60px] md:w-[70px] md:h-[60px] rounded-xl flex justify-center items-center'>
                       <Image 
                       src="https://medeasy.health/icons/footer-messages.svg"
                       width={30}
@@ -41,7 +41,43 @@ const FooterPage = () => {
 
 
         {/* line 2  */}
+        <div className='flex justify-center my-4'>
+            <div className='w-[450px] border'>
+                <p className='text-xl font-semibold'>About us</p>
+            </div>
+
+            <div className='w-[250px] border'>
+                <p>Email:support@medeasy.health</p>
+            </div>
+
+            <div className='w-[250px] border'>
+            <p>Level:3, Road: 14,C 180/6,</p>
+            </div>
+           </div>
+
+
+
+           {/* line 4  */}
+           <div className='flex justify-center my-4'>
+            <div className='w-[450px] border'>
+                <p className='text-lg font-semibold'>Return and Refund Policy</p>
+            </div>
+
+            <div className='w-[250px] border'>
+                <p>01844671112</p>
+                <p>01774671112</p>
+            </div>
+
+            <div className='w-[250px] border'>
+            <p>Trade License:</p>
+            <p>TRAD/DNCC/131766/2022</p>
+            </div>
+           </div>
         
+
+
+
+
 
 
         </div>
