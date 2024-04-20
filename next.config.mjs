09cb314b3cc9fd9da616d069e68c2dca3428a396
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-04-20 20:59:10 +0600

    sidebar done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'medeasy.health',
+            port: '',
+            pathname: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+
+    
+
   return (
     <div>
        <h2 className='text-red-500 text-2xl font-medium'>Home page </h2>
+       <p>homeeeeeeeeeeee</p>
     </div>
   )
 }
